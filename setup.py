74c42e2f1a035ce08462426e5b674c765a8cac5d
Syed Rakib Al Hasan <syed.rakib.al.hasan@gmail.com>
2016-05-25 06:26:51 +0600

    Add setup files for publishing on PyPI

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'rsf_pyrmissions',
+  packages = ['rsf_pyrmissions'],
+  version = '0.1',
+  description = 'A Ridiculously Straight Forward Permissioning system (aka Access Control)',
+  author = 'Syed Rakib Al Hasan',
+  author_email = 'syed.rakib.al.hasan@gmail.com',
+  url = 'https://github.com/syedrakib/rsf-pyrmissions',
+  download_url = 'https://github.com/syedrakib/rsf-pyrmissions/tarball/0.1', # I'll explain this in a second
+  keywords = ['ridiculous', 'permission', 'acl', 'access', 'control', 'rsf'],
+  classifiers = [],
+)
