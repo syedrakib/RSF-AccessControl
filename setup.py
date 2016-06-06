51de3b04a1305dcc13be4993588521f423ad454a
Syed Rakib Al Hasan <syed.rakib.al.hasan@gmail.com>
2016-06-07 03:07:32 +0600

    Ready for 6th release

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
   name = 'rsf_pyrmissions',
   packages = ['rsf_pyrmissions'],
-  version = '0.5.2',
+  version = '0.6.2',
   description = 'A Ridiculously Straight Forward Permissioning system (aka Access Control) for python projects',
   author = 'Syed Rakib Al Hasan',
   author_email = 'syed.rakib.al.hasan@gmail.com',
   url = 'https://github.com/syedrakib/rsf-pyrmissions',
-  download_url = 'https://github.com/syedrakib/rsf-pyrmissions/tarball/0.5.2',
+  download_url = 'https://github.com/syedrakib/rsf-pyrmissions/tarball/0.6.2',
   keywords = ['ridiculous', 'permission', 'acl', 'access', 'control', 'rsf', 'role', 'user', 'privileges'],
   classifiers = [],
 )
